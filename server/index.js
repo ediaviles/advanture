e88d2f84e89c63d8c3bca88252522c0f22c55bb5
Edison Aviles <eaviles@edisons-air.wifi.local.cmu.edu>
2024-03-14 13:38:08 -0400

    Added packages

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
-// server/index.js
-
 const express = require("express");
-
-const PORT = process.env.PORT || 3001;
-
 const app = express();
+const PORT = process.env.PORT || 3001;
+const cors = require('cors')
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+app.use(cors());
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/local");
+console.log(mongoose.connection.readyState)
+
+// TODO: GET islands by user
+
+// TODO: GET islands by tags
+
+// TODO: GET user information
+
+// TODO: POST new user
+
+// TODO: POST new island
